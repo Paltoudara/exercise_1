bfb951e050f1b591259421428f9549ab8072968e
panagiotis dimitropoulos <162046043+Paltoudara@users.noreply.github.com>
2026-04-28 06:37:27 +0300

    Update trapezoid_code_parallel.cpp

--- trapezoid_code_parallel.cpp
+++ trapezoid_code_parallel.cpp
@@ -2,7 +2,7 @@
 #include<chrono>
 #include<cmath>
 #include<numbers>
-#define NUM_THREADS 1
+#define NUM_THREADS 4
 constexpr int n = 1000;
 constexpr double a = 0.0;
 constexpr double b = std::numbers::pi;
@@ -14,34 +14,17 @@ inline double function_f(const double x)noexcept {
 int main() {
 	//parallel part
 	double area{};
-	auto start_1 = std::chrono::high_resolution_clock::now();
+	auto start = std::chrono::high_resolution_clock::now();
 	#pragma omp parallel for  reduction(+:area) num_threads(NUM_THREADS)
 	for (int i = 0;i < n;i++) {
 		double hprev = a + (double)i * h;
 		double hcurr = a + (double)(i + 1) * h;
 		area += 0.5 * (function_f(hprev) + function_f(hcurr)) * h;
 	}//for
-	auto end_1 = std::chrono::high_resolution_clock::now();
-	double parallel_time=std::chrono::duration<double>(end_1 - start_1).count();
+	auto end = std::chrono::high_resolution_clock::now();
+	double parallel_time=std::chrono::duration<double>(end - start).count();
 	std::cout <<"Area :" << area << '\n';
+	std::cout <<"Correct Area :" << correct << '\n';
+	std::cout <<"parallel :" << parallel_time << '\n';
 	//serial part
-
-
-	area = 0.0;
-	auto start_2 = std::chrono::high_resolution_clock::now();
-	for (int i = 0;i < n;i++) {
-		double hprev = a + (double)i * h;
-		double hcurr = a + (double)(i + 1) * h;
-		area += 0.5 * (function_f(hprev) + function_f(hcurr)) * h;
-	}//for
-	auto end_2 = std::chrono::high_resolution_clock::now();
-	double serial_time=std::chrono::duration<double>(end_2 - start_2).count();
-	std::cout << "Area :" << area << "\n\n\n";
-	//
-
-	double speedup = serial_time / parallel_time;
-	std::cout <<"Serial: " << serial_time << '\n';
-	std::cout <<"Parallel: " << parallel_time << '\n';
-	std::cout << "Speedup: " << speedup << '\n';
-	std::cout << "Efficiency: " << (speedup / NUM_THREADS) * 100 << '\n';
 }
