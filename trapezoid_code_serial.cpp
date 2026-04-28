9439d22e2f50a5a655920106ea181bdad3af7071
panagiotis dimitropoulos <162046043+Paltoudara@users.noreply.github.com>
2026-04-28 06:39:18 +0300

    Update trapezoid_code_serial.cpp

--- trapezoid_code_serial.cpp
+++ trapezoid_code_serial.cpp
@@ -11,7 +11,7 @@ inline double function_f(const double x)noexcept {
 	return x * x * std::sin(x);
 }
 int main() {
-	//parallel part
+	
 	double area{};
 	auto start = std::chrono::high_resolution_clock::now();
 	for (int i = 0;i < n;i++) {
